a49bf3be5124952c6547d31733a20418c3bad40d
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-10-07 19:20:00 +0700

    add comments

--- app/src/main/java/com/sgc/graphslibrary/model/PieChartModel.java
+++ app/src/main/java/com/sgc/graphslibrary/model/PieChartModel.java
@@ -3,8 +3,19 @@ package com.sgc.graphslibrary.model;
 import android.graphics.Color;
 
 public class PieChartModel {
+    /**
+     *percentage of space occupied by a segment diagram
+     */
     private int percentageSpace;
+
+    /**
+     * chart segment color
+     */
     private int colorDedicatedSpace;
+
+    /**
+     * description segment pie chart
+     */
     private String text;
 
     public PieChartModel(int percentageSpace, int colorDedicatedSpace) {
@@ -19,26 +30,44 @@ public class PieChartModel {
     }
 
 
+    /**
+     * @return chart segment color
+     */
     public int getColorDedicatedSpace() {
         return colorDedicatedSpace;
     }
 
+    /**
+     * @param colorDedicatedSpace chart segment color
+     */
     public void setColorDedicatedSpace(int colorDedicatedSpace) {
         this.colorDedicatedSpace = colorDedicatedSpace;
     }
 
+    /**
+     * @return percentage of space occupied by a segment diagram
+     */
     public int getPercentageSpace() {
         return percentageSpace;
     }
 
+    /**
+     * @param percentageSpace percentage of space occupied by a segment diagram
+     */
     public void setPercentageSpace(int percentageSpace) {
         this.percentageSpace = percentageSpace;
     }
 
+    /**
+     * @return description segment pie chart
+     */
     public String getText() {
         return text;
     }
 
+    /**
+     * @param text description segment pie chart
+     */
     public void setText(String text) {
         this.text = text;
     }

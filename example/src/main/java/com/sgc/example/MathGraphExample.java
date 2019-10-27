9e5355c871898cc44f03d13b87cb201e730123b6
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-10-27 11:55:13 +0700

    Change example

--- example/src/main/java/com/sgc/example/MathGraphExample.java
+++ example/src/main/java/com/sgc/example/MathGraphExample.java
@@ -40,29 +40,13 @@ public class MathGraphExample extends Fragment {
         View view = inflater.inflate(R.layout.fragment_math_graph_example, container, false);
         unbinder = ButterKnife.bind(this, view);
         ArrayList<MathData> graphs = new ArrayList<>();
-        MathData mathData1 = new MathData(100, new MathData.MathFunctionInterface() {
+        MathData mathData3 = new MathData(50, new MathData.MathFunctionInterface() {
             @Override
             public float function(float x) {
-                return -x;
-            }
-        });
-        mathData1.setColorGraph(Color.GREEN);
-        MathData mathData2 = new MathData(100, new MathData.MathFunctionInterface() {
-            @Override
-            public float function(float x) {
-                return x;
-            }
-        });
-        mathData2.setColorGraph(Color.RED);
-        MathData mathData3 = new MathData(100, new MathData.MathFunctionInterface() {
-            @Override
-            public float function(float x) {
-                return x * x;
+                return (float) Math.sin(x);
             }
         });
         mathData3.setColorGraph(Color.BLUE);
-        graphs.add(mathData1);
-        graphs.add(mathData2);
         graphs.add(mathData3);
         math.setFunctions(graphs);
 

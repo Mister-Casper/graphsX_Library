8961ff37730d8e9116c4c2f4a941c6bf6c050c95
Kirill Kozinets <kirillkozinec123@gmail.com>
2019-11-12 18:44:29 +0700

    change example

--- example/src/main/java/com/sgc/example/BarChartExample.java
+++ example/src/main/java/com/sgc/example/BarChartExample.java
@@ -9,8 +9,8 @@ import android.view.ViewGroup;
 
 import com.sgc.graphslibrary.data.BarChartData;
 import com.sgc.graphslibrary.data.GroupBarChartData;
-import com.sgc.graphslibrary.diagram.HorizontalBarChart;
 import com.sgc.graphslibrary.diagram.VerticalBarChart;
+import com.sgc.graphslibrary.legend.LegendView;
 
 import java.util.ArrayList;
 
@@ -25,8 +25,9 @@ import butterknife.Unbinder;
  */
 public class BarChartExample extends Fragment {
 
-    @BindView(R.id.barChart1)
-    HorizontalBarChart barChart1;
+
+    @BindView(R.id.legend)
+    LegendView legend;
     private Unbinder unbinder;
 
     @BindView(R.id.barChart)
@@ -42,31 +43,46 @@ public class BarChartExample extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_bar_chart_example, container, false);
         unbinder = ButterKnife.bind(this, view);
+
+        barChart.setData(getGroupBarChartData());
+        legend.connectToView(barChart);
+
+        return view;
+    }
+
+    int blue = Color.rgb(1, 87, 155);
+    int violet = Color.rgb(74, 20, 140);
+    int green = Color.rgb(27, 94, 32);
+    int orange = Color.rgb(230, 81, 0);
+    String blueDescription = "A";
+    String violetDescription = "B";
+    String greenDescription = "C";
+    String orangeDescription = "D";
+
+
+    private ArrayList<GroupBarChartData> getGroupBarChartData() {
         ArrayList<BarChartData> data = new ArrayList<>();
-        data.add(new BarChartData(Color.rgb(1, 87, 155), 10));
-        data.add(new BarChartData(Color.rgb(74, 20, 140), 50));
-        data.add(new BarChartData(Color.rgb(27, 94, 32), 15));
-        data.add(new BarChartData(Color.rgb(230, 81, 0), 10));
+        data.add(new BarChartData(blue, 10,blueDescription));
+        data.add(new BarChartData(violet, 50,violetDescription));
+        data.add(new BarChartData(green, 15,greenDescription));
+        data.add(new BarChartData(orange, 10,orangeDescription));
 
         ArrayList<BarChartData> data1 = new ArrayList<>();
-        data1.add(new BarChartData(Color.rgb(230, 81, 0), 10));
-        data1.add(new BarChartData(Color.rgb(27, 94, 32), 15));
-        data1.add(new BarChartData(Color.rgb(1, 87, 155), 15));
-        data1.add(new BarChartData(Color.rgb(74, 20, 140), 40));
+        data1.add(new BarChartData(orange, 10,orangeDescription));
+        data1.add(new BarChartData(green, 15,greenDescription));
+        data1.add(new BarChartData(blue, 15,blueDescription));
+        data1.add(new BarChartData(violet, 40,violetDescription));
 
         ArrayList<BarChartData> data2 = new ArrayList<>();
-        data2.add(new BarChartData(Color.rgb(230, 81, 0), 10));
+        data2.add(new BarChartData(orange, 10,orangeDescription));
 
         ArrayList<GroupBarChartData> groups = new ArrayList<>();
         groups.add(new GroupBarChartData(data, GroupBarChartData.locationHorizontal, "Автомобили"));
         groups.add(new GroupBarChartData(data1, GroupBarChartData.locationVertical, "Корабли"));
-        groups.add(new GroupBarChartData(data2, GroupBarChartData.locationHorizontal, "Корабли"));
-        groups.add(new GroupBarChartData(data1, GroupBarChartData.locationVertical, "Корабли"));
+        groups.add(new GroupBarChartData(data2, GroupBarChartData.locationHorizontal, "Самолеты"));
+        groups.add(new GroupBarChartData(data1, GroupBarChartData.locationVertical, "Велосипеды"));
 
-        barChart.setData(groups);
-        barChart1.setData(groups);
-
-        return view;
+        return groups;
     }
 
 }
